b7bf611c92c934aee4c2136fad42fa046faa27cb
Matheus V <matheusvrgm@gmail.com>
2020-06-09 23:59:07 -0300

    Started implementing Bottom Tabs Bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,89 @@
 import 'react-native-gesture-handler';
 import React from 'react';
-import {View, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {NavigationContainer} from '@react-navigation/native';
+import Home from './Pages/Home/';
+import Icon from 'react-native-vector-icons/Feather';
+import {shade} from 'polished';
+const BottomTab = createBottomTabNavigator();
 
 // import { Container } from './styles';
 
 const App: React.FC = () => {
   return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
+    <NavigationContainer>
+      <BottomTab.Navigator
+        initialRouteName="Home"
+        tabBarOptions={{showLabel: false, activeTintColor: '#000'}}>
+        <BottomTab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Icon
+                name="home"
+                color={focused ? color : '#000'}
+                size={focused ? 38 : 32}
+              />
+            ),
+          }}
+        />
+
+        <BottomTab.Screen
+          name="Home 2"
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Icon
+                name="search"
+                color={focused ? color : shade('0.2', color)}
+                size={focused ? 38 : 32}
+              />
+            ),
+          }}
+        />
+
+        <BottomTab.Screen
+          name="Home 3"
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Icon
+                name="plus-circle"
+                color={focused ? color : shade('0.2', color)}
+                size={focused ? 38 : 32}
+              />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Home 4"
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Icon
+                name="heart"
+                color={focused ? color : shade('0.2', color)}
+                size={focused ? 38 : 32}
+              />
+            ),
+          }}
+        />
+        <BottomTab.Screen
+          name="Home 5"
+          component={Home}
+          options={{
+            tabBarIcon: ({color, size, focused}) => (
+              <Icon
+                name="circle"
+                color={focused ? color : shade('0.2', color)}
+                size={focused ? 38 : 32}
+              />
+            ),
+          }}
+        />
+      </BottomTab.Navigator>
+    </NavigationContainer>
   );
 };
 
---
+++ src/Pages/Home/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+const Home: React.FC = () => {
+  return (
+    <>
+      <View>
+        <Text>Hello World!</Text>
+      </View>
+    </>
+  );
+};
+export default Home;
