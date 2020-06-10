f592ad21669e09a0c14dbe13295eaeabef42438c
Matheus V <matheusvrgm@gmail.com>
2020-06-09 23:03:46 -0300

    First Commit - App Base

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {View, Text} from 'react-native';
+
+// import { Container } from './styles';
+
+const App: React.FC = () => {
+  return (
+    <View>
+      <Text>Hello World!</Text>
+    </View>
+  );
+};
+
+export default App;
